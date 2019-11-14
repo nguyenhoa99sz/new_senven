330830e645f4ade7aa078799f635ef3197cef80b
nguyenhoa99sz <nguyennguyenhoa9c@gmail.com>
2019-11-14 18:39:22 +0700

    commit

--- module-4/4-content.php
+++ module-4/4-content.php
@@ -30,12 +30,36 @@
                 </ul>
             </div>
         </div>
-        <div class="row">
-            <div class="adudu-row">
-                <div class="col-md-2">a</div>
-                <div class="col-md-6">a</div>
-                <div class="col-md-4">a</div>
+    </div>
+    <div class="container ctn2">
+        <div class="form-search-custom">
+            <div class="row">
+                <div class="col-xs-2">
+                    <img src="images/bd886d7ccc6f8dd8db17e841233c9656.jpg" alt="anh" class="img-responsive img-mm">
+                </div>
+                <div class="col-xs-6">
+                    <div class="form-input">
+                        <input type="text" class="ipt-css" placeholder="Seach"><button class="btn-seach">Search</button>
+                        <div class="in-input">
+                            <a href="#">Categories</a>
+                            <ul class="nav-menu">
+                                <li>
+                                    <a href="#">Kimuchi 2</a>
+                                </li>
+                                <li>
+                                    <a href="#">Kimuchi 3</a>
+                                </li>
+                                <li>
+                                    <a href="#">Kimuchi 4</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-xs-4">a</div>
             </div>
+
         </div>
     </div>
 </div>
