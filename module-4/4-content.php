d612fc6a9a2f0eb9e75f3b0f2a27d1beff705ffa
nguyenhoa99sz <nguyennguyenhoa9c@gmail.com>
2019-11-14 17:07:32 +0700

    cmomit

---
+++ module-4/4-content.php
@@ -0,0 +1,41 @@
+<div class="type-4">
+    <div class="container">
+        <div class="nav-custom">
+            <div class="nav-title-left navbar-left">
+                <span>Get 20% off on Selected Items</span>
+            </div>
+            <div class="nav nav-right-menu navbar-right">
+                <ul>
+                    <li>
+                        <a href="#">My Accound <span class="caret"></span> |</a>
+                        <ul class="nav-menu">
+                            <li>
+                                <a href="#">Kimuchi 2</a>
+                            </li>
+                            <li>
+                                <a href="#">Kimuchi 3</a>
+                            </li>
+                            <li>
+                                <a href="#">Kimuchi 4</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Curren Ly |</a>
+
+                    </li>
+                    <li>
+                        <a href="#">Laguage |</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="adudu-row">
+                <div class="col-md-2">a</div>
+                <div class="col-md-6">a</div>
+                <div class="col-md-4">a</div>
+            </div>
+        </div>
+    </div>
+</div>
