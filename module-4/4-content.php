23f39233afe452f50a0e7d446c93574a19c22c20
nguyenhoa99sz <nguyennguyenhoa9c@gmail.com>
2019-11-14 19:34:41 +0700

    commit

--- module-4/4-content.php
+++ module-4/4-content.php
@@ -41,7 +41,7 @@
                     <div class="form-input">
                         <input type="text" class="ipt-css" placeholder="Seach"><button class="btn-seach">Search</button>
                         <div class="in-input">
-                            <a href="#">Categories</a>
+                            <a href="#" class="adu-cts">Categories</a>
                             <ul class="nav-menu">
                                 <li>
                                     <a href="#">Kimuchi 2</a>
@@ -57,9 +57,31 @@
                     </div>
 
                 </div>
-                <div class="col-xs-4">a</div>
-            </div>
+                <div class="col-xs-4">
+                    <ul class="fawpro">
+                        <li>
+							<i class="fa fa-youtube"></i>
+							<a href="#">Youtube</a>
+                        </li>
 
+                        <li>
+							<i class="fa fa-search"></i>
+							<a href="#">Search</a>
+                        </li>
+                        <li>
+							<i class="fa fa-car"></i>
+							<a href="#">My Car</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
+    </div>
+    <div class="container addad">
+        <span class="addad">Special</span>
+        <span class="addad">Contact</span>
+        <span class="addad">SiteMaps</span>
+        <span class="addad">Brands</span>
+        <span class="addad">Return</span>
     </div>
 </div>
